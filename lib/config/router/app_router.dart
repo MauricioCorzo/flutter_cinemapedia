f2cb5622a1748efc669e893e8d5e881a733889a8
MauricioCorzo <mauricio.corzo@yahoo.com>
2024-10-28 15:28:33 -0300

    StatefulShellRoute config

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,31 +1,98 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   initialLocation: "/",
   routes: [
-    GoRoute(
-        path: "/",
-        name: HomeScreen.name,
-        pageBuilder: (context, state) {
-          //Custom page transition example
-          return cupertinolikePageTransition(child: const HomeScreen());
-        },
-        routes: [
+    // GoRoute(
+    //     path: "/",
+    //     name: HomeScreen.name,
+    //     pageBuilder: (context, state) {
+    //       //Custom page transition example
+    //       return cupertinolikePageTransition(
+    //         child: const HomeScreen(
+    //           childView: HomeView(),
+    //         ),
+    //       );
+    //     },
+    //     routes: [
+    //       GoRoute(
+    //         path: "movie/:id",
+    //         name: MovieScreen.name,
+    //         pageBuilder: (context, state) {
+    //           //Custom page transition example
+    //           return cupertinolikePageTransition(
+    //             child: MovieScreen(
+    //               movieId: state.pathParameters["id"] ?? "no-id",
+    //             ),
+    //           );
+    //         },
+    //       ),
+    //     ]),
+
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, statefulNavigationShell) {
+        return HomeScreen(childView: statefulNavigationShell);
+      },
+      branches: [
+        StatefulShellBranch(routes: [
           GoRoute(
-            path: "movie/:id",
-            name: MovieScreen.name,
+            path: "/",
             pageBuilder: (context, state) {
-              //Custom page transition example
-              return cupertinolikePageTransition(
-                child: MovieScreen(
-                  movieId: state.pathParameters["id"] ?? "no-id",
-                ),
-              );
+              return cupertinolikePageTransition(child: const HomeView());
+            },
+
+            //Si quiero mantener el BottomNavigationBar en esta pantalla
+            //tengo que declararlo aqui y no afuera
+
+            // routes: [
+            // GoRoute(
+            //   path: "movie/:id",
+            //   name: MovieScreen.name,
+            //   pageBuilder: (context, state) {
+            //     //Custom page transition example
+            //     return cupertinolikePageTransition(
+            //       child: MovieScreen(
+            //         movieId: state.pathParameters["id"] ?? "no-id",
+            //       ),
+            //     );
+            //   },
+            // ),
+            // ],
+          ),
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(
+            path: "/categories",
+            pageBuilder: (context, state) {
+              return cupertinolikePageTransition(child: const CategoriesView());
             },
           ),
         ]),
+        StatefulShellBranch(routes: [
+          GoRoute(
+            path: "/favorites",
+            pageBuilder: (context, state) {
+              return cupertinolikePageTransition(child: const FavoritesView());
+            },
+          ),
+        ])
+      ],
+    ),
+    GoRoute(
+      path: "/movie/:id",
+      name: MovieScreen.name,
+      pageBuilder: (context, state) {
+        //Custom page transition example
+        return cupertinolikePageTransition(
+          child: MovieScreen(
+            movieId: state.pathParameters["id"] ?? "no-id",
+          ),
+        );
+      },
+    ),
   ],
 );
 
